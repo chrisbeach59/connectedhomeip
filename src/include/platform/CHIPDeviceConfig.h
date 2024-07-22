ea9a12cf5709cfe3854269fda6b78ce13ccf469b
Chris Beach <chrisbeach59@gmail.com>
2024-07-22 10:48:16 -0400

    DAC updates for passing SVE

--- src/include/platform/CHIPDeviceConfig.h
+++ src/include/platform/CHIPDeviceConfig.h
@@ -154,7 +154,7 @@
  * (see src/credentials/examples/DeviceAttestationCredsExample.cpp).
  */
 #ifndef CHIP_DEVICE_CONFIG_DEVICE_VENDOR_ID
-#define CHIP_DEVICE_CONFIG_DEVICE_VENDOR_ID 0xFFF1
+#define CHIP_DEVICE_CONFIG_DEVICE_VENDOR_ID 0x131A
 #endif
 
 /**
@@ -177,7 +177,7 @@
  * (see src/credentials/examples/DeviceAttestationCredsExample.cpp)
  */
 #ifndef CHIP_DEVICE_CONFIG_DEVICE_PRODUCT_ID
-#define CHIP_DEVICE_CONFIG_DEVICE_PRODUCT_ID 0x8001
+#define CHIP_DEVICE_CONFIG_DEVICE_PRODUCT_ID 0x0001
 #endif
 
 /**
